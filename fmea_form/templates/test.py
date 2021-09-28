8cc9816b90101e0c7d5c7648dbe43a4d9dbea54f
Gokukrish <gokul.dcs@pec.edu>
2021-09-28 08:56:08 +0530

    show page added

---
+++ fmea_form/templates/test.py
@@ -0,0 +1,30 @@
+list=["Type",
+"Process Step/Input",
+"Potential Failure Mode",
+"Potential Failure Effects",
+"Severity",
+"PotentialCauses",
+"Occurence",
+"CurrentControls",
+"Detection",
+"RPN",
+"Action Recommended",
+"Responsible",
+"ActionsTaken",
+"Tracker",
+"RiskLevel",
+"Status",
+"Severity",
+"Occurence",
+"Detection",
+"Final RPN",
+]
+
+for i in list:
+    print('<div class="row mb-2">')
+    print('<div class="col-md-3 col-lg-3 font-weight-bold">')
+    print(i)
+    print('</div>')
+    print('<div class="col-md-9 col-lg-9">')
+    print('</div>')
+    print('</div>')
